f2b6bb355bc1c0d6299b110b5e9030df549eda3a
ritu-477 <ritujoharbishnoi@gmail.com>
2024-12-18 14:43:16 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className='container'>
         <div className='flex md:justify-between items-center max-md:flex-col justify-center max-md:gap-2'>
           <p className='text-xs md:leading-7 leading-5 text-soft-indigo md:text-start text-center'>©{currentYear} KeySemantics GmbH | Ramsenburgweg 24, 9100 Herisau, Switzerland</p>
-          <a target='blank' className='text-xs md:leading-7 leading-5 text-soft-indigo underline' href="/">info@keysemantics.ch</a>
+          <a target="_blank" className="text-xs md:leading-7 leading-5 text-soft-indigo underline" href="mailto:info@keysemantics.ch">info@keysemantics.ch</a>
         </div>
       </div>
       <BackToTop/>
